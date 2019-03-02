a7c76febf81cc85517410b6fd6cd8a9f4567ec05
Michael Guymon <michael.guymon@gmail.com>
2019-03-01 22:06:34 -0500

    Add codecov support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,14 @@ require 'bundler/setup'
 require 'pry'
 require 'paseto'
 
+if ENV['CODECOV']
+  require 'simplecov'
+  SimpleCov.start
+
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
